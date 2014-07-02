d328910b566d3839205b05a096b0326722e0d0fd
Jon Perl <perl.jonathan@gmail.com>
2014-07-01 21:21:59 -0400

    Init commit

---
+++ package.js
@@ -0,0 +1,7 @@
+Package.describe({
+  summary: "nameparser repackaged for Meteor"
+});
+
+Package.on_use(function (api, where) {
+  api.add_files('nameparser/parse-names.js', 'client', { bare: true });
+});
